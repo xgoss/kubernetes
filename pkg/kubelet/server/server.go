1bce4b3e737dbab264f1572ef0b07f87f944bab9
Maciej Szulik <maszulik@redhat.com>
2015-12-21 13:18:06 +0100

    UPSTREAM: <carry>: add kubelet timeouts
    :100644 100644 c561ddb... 17ef5df... M	pkg/kubelet/server.go

--- pkg/kubelet/server/server.go
+++ pkg/kubelet/server/server.go
@@ -134,6 +134,8 @@ func ListenAndServeKubeletServer(
 	s := &http.Server{
 		Addr:           net.JoinHostPort(address.String(), strconv.FormatUint(uint64(port), 10)),
 		Handler:        &handler,
+		ReadTimeout:    60 * time.Minute,
+		WriteTimeout:   60 * time.Minute,
 		MaxHeaderBytes: 1 << 20,
 	}
 	if tlsOptions != nil {
@@ -155,6 +157,8 @@ func ListenAndServeKubeletReadOnlyServer(host HostInterface, resourceAnalyzer st
 	server := &http.Server{
 		Addr:           net.JoinHostPort(address.String(), strconv.FormatUint(uint64(port), 10)),
 		Handler:        &s,
+		ReadTimeout:    60 * time.Minute,
+		WriteTimeout:   60 * time.Minute,
 		MaxHeaderBytes: 1 << 20,
 	}
 	glog.Fatal(server.ListenAndServe())
