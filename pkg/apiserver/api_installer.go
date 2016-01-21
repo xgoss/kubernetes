1101efb15da9f4e46923e5796525e8f6af734aef
liggitt <jliggitt@redhat.com>
2016-01-20 23:21:34 -0500

    UPSTREAM: <carry>: allow specific, skewed group/versions
    :100644 100644 2328ffc... a876a74... M	pkg/apiserver/api_installer.go
    :100644 100644 471bcd5... 2c8c2f9... M	pkg/apiserver/apiserver.go

--- pkg/apiserver/api_installer.go
+++ pkg/apiserver/api_installer.go
@@ -142,6 +142,7 @@ func (a *APIInstaller) registerResourceHandlers(path string, storage rest.Storag
 	for _, fqKind := range fqKinds {
 		if fqKind.Group == a.group.GroupVersion.Group {
 			fqKindToRegister = fqKind
+			fqKindToRegister.Version = a.group.GroupVersion.Version
 			break
 		}
 
@@ -153,19 +154,24 @@ func (a *APIInstaller) registerResourceHandlers(path string, storage rest.Storag
 		}
 	}
 
+	// Allow overriding the group/version/kind for a given path
+	if nonDefaultGroupVersionKind, exists := a.group.NonDefaultGroupVersionKinds[path]; exists {
+		fqKindToRegister = nonDefaultGroupVersionKind
+	}
+
 	kind := fqKindToRegister.Kind
 
 	if fqKindToRegister.IsEmpty() {
 		return nil, fmt.Errorf("unable to locate fully qualified kind for %v: found %v when registering for %v", reflect.TypeOf(object), fqKinds, a.group.GroupVersion)
 	}
 
-	versionedPtr, err := a.group.Creater.New(a.group.GroupVersion.WithKind(kind))
+	versionedPtr, err := a.group.Creater.New(fqKindToRegister)
 	if err != nil {
 		return nil, err
 	}
 	versionedObject := indirectArbitraryPointer(versionedPtr)
 
-	mapping, err := a.group.Mapper.RESTMapping(fqKindToRegister.GroupKind(), a.group.GroupVersion.Version)
+	mapping, err := a.group.Mapper.RESTMapping(fqKindToRegister.GroupKind(), fqKindToRegister.Version)
 	if err != nil {
 		return nil, err
 	}
@@ -454,7 +460,7 @@ func (a *APIInstaller) registerResourceHandlers(path string, storage rest.Storag
 
 		Resource:    a.group.GroupVersion.WithResource(resource),
 		Subresource: subresource,
-		Kind:        a.group.GroupVersion.WithKind(kind),
+		Kind:        fqKindToRegister,
 	}
 	for _, action := range actions {
 		reqScope.Namer = action.Namer
