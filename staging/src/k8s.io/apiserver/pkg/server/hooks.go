3658f4e7201cd28002a2146a260caf975e66888c
deads2k <deads@redhat.com>
2017-03-03 10:14:23 -0500

    UPSTREAM: <drop>: wait for loopback permissions, remove after updating loopback authenticator
    :100644 100644 4400c5ded1... 2facf87feb... M	pkg/genericapiserver/hooks.go

--- staging/src/k8s.io/apiserver/pkg/server/hooks.go
+++ staging/src/k8s.io/apiserver/pkg/server/hooks.go
@@ -20,12 +20,16 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/golang/glog"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apiserver/pkg/server/healthz"
 	restclient "k8s.io/client-go/rest"
+	coreclient "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/typed/core/internalversion"
 )
 
 // PostStartHookFunc is a function that is called after the server has started.
@@ -92,6 +96,21 @@ func (s *GenericAPIServer) RunPostStartHooks() {
 	s.postStartHooksCalled = true
 
 	context := PostStartHookContext{LoopbackClientConfig: s.LoopbackClientConfig}
+	// we need to wait until our loopback client has enough rights to actually loopback
+	// the core APIs cannot be disabled, so we'll use that as a proxy for the information instead
+	// of attempting a SAR (which can be disabled, though that would be a very bad idea)
+	// TODO remove once openshift starts using the kube loopback connection.
+	err := wait.PollImmediate(30*time.Millisecond, 30*time.Second, func() (bool, error) {
+		client := coreclient.NewForConfigOrDie(s.LoopbackClientConfig)
+		// we can have other errors in our level, like etcd not being up. Treat them all the same
+		if _, err := client.Namespaces().List(metav1.ListOptions{}); err != nil {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		glog.Fatalf("LoopbackClient was unable to list namespaces: %v", err)
+	}
 
 	for hookName, hookEntry := range s.postStartHooks {
 		go runPostStartHook(hookName, hookEntry, context)
